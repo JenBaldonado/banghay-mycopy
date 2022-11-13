6e0db592fbbe6860e6091cdb681f055f2d061595
Jenza <spotty0326@gmail.com>
2022-11-13 21:14:31 +0800

    added code to the migrations tables

--- database/migrations/2022_11_06_045853_create_banghays_table.php
+++ database/migrations/2022_11_06_045853_create_banghays_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('banghays');
         Schema::create('banghays', function (Blueprint $table) {
             $table->id();
             $table->string('gradelevel');
