e2cec063d91a6f234b78e87ec3748f5e6d07d607
JenBaldonado <spotty0326@gmail.com>
2022-11-14 10:06:24 +0800

    deleted the extra route in web.php

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
